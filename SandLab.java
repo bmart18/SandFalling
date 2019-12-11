fffaa3a0861fefcef272ff8702c5dd7b8991a3fd
Brad Martin <mbrad664@gmail.com>
2019-12-11 17:53:23 -0500

    Updated
    Gave sand's straight down physics to a new element named dirt, then gave sand a more realistic physics that falls into a pyramid.
    Metal can now rust into rusted metal which then can be destroyed.

--- SandLab.java
+++ SandLab.java
@@ -5,17 +5,18 @@ public class SandLab
 {
   public static void main(String[] args)
   {
-    SandLab lab = new SandLab(200, 200);
+    SandLab lab = new SandLab(120, 80);
     lab.run();
   }
   
-  //add constants for particle types here
   public static final int EMPTY = 0;
   public static final int METAL = 1;
-  public static final int SAND = 2;
+  public static final int DIRT = 2;
   public static final int WATER = 3;
-  // task 4 -- Add a SAND particle type
-  // task 6 -- Add a WATER particle type
+  public static final int RUSTEDMETAL = 4;
+  public static final int SAND = 5;
+  public int rustCounter = 0;
+  public int rustDestroyer = 0;
 
   
   //do not add any more fields
@@ -25,29 +26,22 @@ public class SandLab
   public SandLab(int numRows, int numCols)
   {
     String[] names;
-    names = new String[4];    // will need to modify this for tasks 4, 6
+    names = new String[6]; 
     names[EMPTY] = "Empty";
     names[METAL] = "Metal";
-    names[SAND] = "Sand";
+    names[DIRT] = "Dirt";
     names[WATER] = "Water";
-    
-    // task 4 -- Add a SAND particle type
-    // task 6 -- Add a WATER particle type
+    names[RUSTEDMETAL] = "Rusted Metal";
+    names[SAND] = "Sand";
     display = new SandDisplay("Falling Sand", numRows, numCols, names);
     
-    // task 1
-    // Insert code to initialize the grid field to refer to a 
-    // 2-dimensional array of the same dimensions.
     grid = new int[numRows][numCols];
      
   }
   
-  //called when the user clicks on a location using the given tool
   private void locationClicked(int row, int col, int tool)
   {
-    // task 2
-    // Write code to store this value representing the given tool 
-    // in the corresponding position of the grid array
+  
     grid[row][col] = tool;
     
     
@@ -56,20 +50,23 @@ public class SandLab
   //copies each element of grid into the display
   public void updateDisplay()
   {
-    // task 3
-    // Complete this method so that empty locations are shown in one 
-    // color (probably black) and metal locations are shown in 
-    // another color (probably gray).
     Color black = new Color(0,0,0);
     Color gray = new Color(100,100,100);
+    Color rust = new Color(183, 65, 14);
+    Color brown = new Color(101,67,33);
     for(int i = 0; i < grid.length; i++){
       for(int j = 0; j <grid[0].length; j++){
          if(grid[i][j] == 1){
             display.setColor(i,j,gray);
          }else if(grid[i][j] == 2){
-         display.setColor(i,j,Color.yellow);
+         display.setColor(i,j,brown);
          }else if(grid[i][j] == 3){
          display.setColor(i,j,Color.blue);
+         }else if(grid[i][j]==4){
+         display.setColor(i,j, rust);
+         }else if(grid[i][j]==5){
+         
+         display.setColor(i,j,Color.yellow);
          }else{
          display.setColor(i,j,black); 
          }
@@ -79,23 +76,77 @@ public class SandLab
     
     
   }
-
-  //called repeatedly.
-  //causes one random particle to maybe do something.
   public void step()
   {
-    // task 5
-    // This method should choose a single random valid location. 
-    // (Do not use a loop.) If that location contains a sand particle 
-    // and the location below it is empty, the particle should move 
-    // down one row. (Metal particles will never move.)
+
     double dRandomOne = Math.random()*grid.length-1;
     double dRandomTwo = Math.random()*grid[0].length;
     
     int randomOne = (int)dRandomOne;
     int randomTwo = (int)dRandomTwo;
+    if(grid[randomOne][randomTwo] == 5){
+      double waterRandom = Math.random();
+      if(waterRandom < 0.33){
+         if(grid[randomOne+1][randomTwo]== 0){
+         grid[randomOne][randomTwo]=0;
+         grid[randomOne+1][randomTwo]=5;
+      }else if(grid[randomOne+1][randomTwo]==3){
+         grid[randomOne][randomTwo] = 3;
+         grid[randomOne+1][randomTwo] = 5;
+      }
+         
+      }else if(waterRandom < 0.66 && randomTwo+1 < grid[0].length){
+         if(grid[randomOne+1][randomTwo+1]== 0){
+         grid[randomOne][randomTwo]=0;
+         grid[randomOne+1][randomTwo+1]=5;
+      }else if(grid[randomOne+1][randomTwo+1]==3){
+         grid[randomOne][randomTwo] = 3;
+         grid[randomOne+1][randomTwo+1] = 5;
+      }
+
+      }else if((randomTwo-1 >= 0)){
+         if(grid[randomOne+1][randomTwo-1]== 0){
+         grid[randomOne][randomTwo]=0;
+         grid[randomOne+1][randomTwo-1]=5;
+      }else if(grid[randomOne+1][randomTwo-1]==3){
+         grid[randomOne][randomTwo] = 3;
+         grid[randomOne+1][randomTwo-1] = 5;
+      }
+
+      }
+
+    }
+    if(grid[randomOne][randomTwo] == 1 && 
+        ((randomOne+1 < grid.length && grid[randomOne +1][randomTwo] == 3) || 
+        (randomOne+1 < grid.length && randomTwo+1 < grid[0].length && grid[randomOne+1][randomTwo+1] == 3) ||
+        (randomOne+1 < grid.length && randomTwo-1 > 0 && grid[randomOne+1][randomTwo-1] == 3) ||
+        (randomTwo+1 < grid[0].length && grid[randomOne][randomTwo+1] == 3) || 
+        (randomTwo-1 > 0 && grid[randomOne][randomTwo-1] == 3) ||  
+        (randomOne-1 > 0 && grid[randomOne -1][randomTwo] == 3) ||
+        (randomOne-1 > 0 && randomTwo+1 < grid[0].length && grid[randomOne-1][randomTwo+1] == 3) || 
+        (randomOne-1 > 0 && randomTwo-1 > 0 && grid[randomOne-1][randomTwo-1] == 3))){
+      rustCounter+=1;
+      if(rustCounter > 4000){
+         grid[randomOne][randomTwo] = 4;
+         rustCounter = 0;
+      }
+    }
+    if(grid[randomOne][randomTwo] == 4 && 
+        ((randomOne+1 < grid.length && grid[randomOne +1][randomTwo] == 3) || 
+        (randomOne+1 < grid.length && randomTwo+1 < grid[0].length && grid[randomOne+1][randomTwo+1] == 3) ||
+        (randomOne+1 < grid.length && randomTwo-1 > 0 && grid[randomOne+1][randomTwo-1] == 3) ||
+        (randomTwo+1 < grid[0].length && grid[randomOne][randomTwo+1] == 3) || 
+        (randomTwo-1 > 0 && grid[randomOne][randomTwo-1] == 3) ||  
+        (randomOne-1 > 0 && grid[randomOne -1][randomTwo] == 3) ||
+        (randomOne-1 > 0 && randomTwo+1 < grid[0].length && grid[randomOne-1][randomTwo+1] == 3) || 
+        (randomOne-1 > 0 && randomTwo-1 > 0 && grid[randomOne-1][randomTwo-1] == 3))){
+      rustDestroyer +=1;
+      if(rustDestroyer > 4000){
+         grid[randomOne][randomTwo]=0;
+         rustDestroyer = 0;
+      }
    // System.out.println(random);
-    //System.out.println(grid[random][random]);
+    }//System.out.println(grid[random][random]);
     if(grid[randomOne][randomTwo] == 2)
     {
       if(grid[randomOne+1][randomTwo]== 0){
